e75faffdb00c7d5be88051fb5ec3b0c3836647ad
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-01-24 10:47:55 -0300

    Test medusa with structs

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+struct Params {
+    uint256 increment;
+    uint256 multiply;
+}
+
 contract Counter {
     uint256 public number;
 
@@ -10,5 +15,10 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function update(Params memory params) public {
+        number += params.increment;
+        number *= params.multiply;
     }
 }
--- test/invariants/TargetFunctions.sol
+++ test/invariants/TargetFunctions.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.20;
 import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
 import {Setup} from "./Setup.sol";
 import {Properties} from "./Properties.sol";
+import {Params} from "@src/Counter.sol";
 
 /**
     echidna . --contract CryticTester --config echidna.yaml
@@ -13,5 +14,9 @@ import {Properties} from "./Properties.sol";
 abstract contract TargetFunctions is Setup, Properties, BaseTargetFunctions {
     function increment() public {
         counter.increment();
+    }
+
+    function update(Params memory params) public {
+        counter.update(params);
     }
 }
